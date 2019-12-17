b531a6ca801b9228f152933d0887ef8e076b0e9a
ewatsonPP <56363044+ewatsonPP@users.noreply.github.com>
2019-12-17 20:55:05 +0000

    Updated stylesheet header footer css new version

--- stylesheets.php
+++ stylesheets.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="mainbody.css">
-         <link rel="stylesheet" href="headerfooter.css">
+         <link rel="stylesheet" href="headerfooter1.css">
               
         
     <script src="https://kit.fontawesome.com/3fa65bdc62.js" crossorigin="anonymous"></script>
